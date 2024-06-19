4b8561634e28ab810e95979579c368c66d2f58c5
Shahmir Shehzad <Shahmirshehzad@gmail.com>
2024-06-19 14:09:21 +0500

    first commit

---
+++ weather_app/weather.js
@@ -0,0 +1,11 @@
+const apiKey = "493c27ac657f7badf4829d5827c031d2";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=bangalore";
+
+async function checkWeather() {
+    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+    var data = await response.json();
+
+    console.log(data);
+}
+
+checkWeather();
